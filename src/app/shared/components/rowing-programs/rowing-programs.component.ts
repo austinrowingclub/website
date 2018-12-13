279fb5a1ab2538d775995ac3a8d56ad3547d82ae
Mikaela Hensen <mikaelahensen@Mikaelas-MacBook-Pro-2.local>
2018-12-13 16:46:51 -0600

    updates programs page to include rowing programs

---
+++ src/app/shared/components/rowing-programs/rowing-programs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rowing-programs',
+  templateUrl: './rowing-programs.component.html',
+  styleUrls: ['./rowing-programs.component.scss']
+})
+export class RowingProgramsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
